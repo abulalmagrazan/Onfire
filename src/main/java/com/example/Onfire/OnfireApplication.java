afca58b7efc040af1e434010924f806044b988a2
abulalmagrazan <135784190+abulalmagrazan@users.noreply.github.com>
2023-07-12 14:53:18 +0700

    Add files via upload

---
+++ src/main/java/com/example/Onfire/OnfireApplication.java
@@ -0,0 +1,13 @@
+package com.example.Onfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnfireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Onfire/OnfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Onfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
